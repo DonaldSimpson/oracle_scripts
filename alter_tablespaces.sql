1c45486d225a6807284e2b793e169db5b6a7d4e2
Donald Simpson <donaldsimpson@gmail.com>
2017-04-25 13:42:53 +0100

    Update alter_tablespaces.sql

--- alter_tablespaces.sql
+++ alter_tablespaces.sql
@@ -1,10 +1,12 @@
 sqlplus / as sysdba
 set linesize 999
 
--- List Oracle Database tablespace files:
+-- useful for when a tablespace limit is reached
+
+-- first, list all Oracle Database tablespace files:
 
 SELECT FILE_NAME as FNAME, TABLESPACE_NAME as TSPACE,BYTES, AUTOEXTENSIBLE as AUTOEX, MAXBYTES as MAXB,INCREMENT_BY as INC FROM DBA_DATA_FILES;
 
--- From the above, get the file name for the Table Space that needs altered, and do something like this:
+-- From the above, note the FILE_NAME for the Table Space that needs altered, and use that to do something like this:
 
--- ALTER DATABASE DATAFILE ‘{/path to above TS file, eg /ora/path/undotbs_0001.dbf}’ AUTOEXTEND ON NEXT 64m MAXSIZE 2G;
+ALTER DATABASE DATAFILE ‘{/path to above TS file, eg /ora/path/undotbs_0001.dbf}’ AUTOEXTEND ON NEXT 64m MAXSIZE 2G;
